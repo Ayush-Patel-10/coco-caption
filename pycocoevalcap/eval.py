340bb957620aaad53de4108ff54c9b1053750519
Ayush Patel <78248225+Ayush-Patel-10@users.noreply.github.com>
2023-11-29 03:01:17 -0500

    Add files via upload

--- pycocoevalcap/eval.py
+++ pycocoevalcap/eval.py
@@ -1,44 +1,28 @@
-__author__ = 'tylin'
-from builtins import dict
-from .tokenizer.ptbtokenizer import PTBTokenizer
-from .bleu.bleu import Bleu
-from .meteor.meteor import Meteor
-from .rouge.rouge import Rouge
-from .cider.cider import Cider
-from .spice.spice import Spice
+#__author__ = 'tylin'
+from pycocoevalcap.tokenizer.ptbtokenizer import PTBTokenizer
+from pycocoevalcap.bleu.bleu import Bleu
+from pycocoevalcap.meteor.meteor import Meteor
+from pycocoevalcap.rouge.rouge import Rouge
+from pycocoevalcap.cider.cider import Cider
+from pycocoevalcap.spice.spice import Spice
 
 class COCOEvalCap:
-    def __init__(self, coco, cocoRes, df):
+    def __init__(self, coco, cocoRes):
+        print('ullu')
         self.evalImgs = []
-        self.eval = dict()
-        self.imgToEval = dict()
+        self.eval = {}
+        self.imgToEval = {}
         self.coco = coco
         self.cocoRes = cocoRes
         self.params = {'image_id': coco.getImgIds()}
 
-        self.gts = None
-        self.res = None
-        self.df = df
+    def evaluate2(self, gts, res):
 
-    def tokenize(self):
-        imgIds = self.params['image_id']
-        # imgIds = self.coco.getImgIds()
-        gts = dict()
-        res = dict()
-        for imgId in imgIds:
-            gts[imgId] = self.coco.imgToAnns[imgId]
-            res[imgId] = self.cocoRes.imgToAnns[imgId]
+        print('tokenizationme...')
 
-        # =================================================
-        # Set up scorers
-        # =================================================
-        print('tokenization...')
         tokenizer = PTBTokenizer()
-        self.gts  = tokenizer.tokenize(gts)
-        self.res = tokenizer.tokenize(res)
-
-    def evaluate(self):
-        self.tokenize()
+        gts  = tokenizer.tokenize(gts)
+        res = tokenizer.tokenize(res)
 
         # =================================================
         # Set up scorers
@@ -48,7 +32,7 @@ class COCOEvalCap:
             (Bleu(4), ["Bleu_1", "Bleu_2", "Bleu_3", "Bleu_4"]),
             (Meteor(),"METEOR"),
             (Rouge(), "ROUGE_L"),
-            (Cider(self.df), "CIDEr"),
+            (Cider(), "CIDEr"),
             (Spice(), "SPICE")
         ]
 
@@ -57,15 +41,70 @@ class COCOEvalCap:
         # =================================================
         for scorer, method in scorers:
             print('computing %s score...'%(scorer.method()))
-            score, scores = scorer.compute_score(self.gts, self.res)
+            score, scores = scorer.compute_score(gts, res)
             if type(method) == list:
                 for sc, scs, m in zip(score, scores, method):
                     self.setEval(sc, m)
-                    self.setImgToEvalImgs(scs, self.gts.keys(), m)
+                    self.setImgToEvalImgs(scs, gts.keys(), m)
                     print("%s: %0.3f"%(m, sc))
             else:
                 self.setEval(score, method)
-                self.setImgToEvalImgs(scores, self.gts.keys(), method)
+                self.setImgToEvalImgs(scores, gts.keys(), method)
+                print("%s: %0.3f"%(method, score))
+        self.setEvalImgs()
+
+    def evaluate(self):
+        print("sdfsadf")
+        print('d')
+        imgIds = self.params['image_id']
+        # imgIds = self.coco.getImgIds()
+        gts = {}
+        res = {}
+        a=0
+        for imgId in imgIds:
+            gts[imgId] = self.coco.imgToAnns[imgId]
+            res[imgId] = self.cocoRes.imgToAnns[imgId]
+            if(a<1):
+              print('')
+              print(self.coco.imgToAnns[imgId])
+              print(self.cocoRes.imgToAnns[imgId])
+              a+=1
+              print('')
+        # =================================================
+        # Set up scorers
+        # =================================================
+        print('tokenizationme...')
+
+        tokenizer = PTBTokenizer()
+        gts  = tokenizer.tokenize(gts)
+        res = tokenizer.tokenize(res)
+
+        # =================================================
+        # Set up scorers
+        # =================================================
+        print('setting up scorers...')
+        scorers = [
+            (Bleu(4), ["Bleu_1", "Bleu_2", "Bleu_3", "Bleu_4"]),
+            (Meteor(),"METEOR"),
+            (Rouge(), "ROUGE_L"),
+            (Cider(), "CIDEr"),
+            (Spice(), "SPICE")
+        ]
+
+        # =================================================
+        # Compute scores
+        # =================================================
+        for scorer, method in scorers:
+            print('computing %s score...'%(scorer.method()))
+            score, scores = scorer.compute_score(gts, res)
+            if type(method) == list:
+                for sc, scs, m in zip(score, scores, method):
+                    self.setEval(sc, m)
+                    self.setImgToEvalImgs(scs, gts.keys(), m)
+                    print("%s: %0.3f"%(m, sc))
+            else:
+                self.setEval(score, method)
+                self.setImgToEvalImgs(scores, gts.keys(), method)
                 print("%s: %0.3f"%(method, score))
         self.setEvalImgs()
 
@@ -75,10 +114,9 @@ class COCOEvalCap:
     def setImgToEvalImgs(self, scores, imgIds, method):
         for imgId, score in zip(imgIds, scores):
             if not imgId in self.imgToEval:
-                self.imgToEval[imgId] = dict()
+                self.imgToEval[imgId] = {}
                 self.imgToEval[imgId]["image_id"] = imgId
             self.imgToEval[imgId][method] = score
 
     def setEvalImgs(self):
         self.evalImgs = [eval for imgId, eval in self.imgToEval.items()]
-
